bd33bf5002f9e185e85894ba7b3ede3fbd554338
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2022-01-20 04:02:10 -0600

    Adding course controller logic, add routes, add cascade delete for courses in bootcamp model

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const { getCourses } = require('../controllers/courses');
+const {
+  getCourses,
+  getSingleCourse,
+  addSingleCourse,
+  updateSingleCourse,
+  deleteSingleCourse,
+} = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addSingleCourse);
+router
+  .route('/:id')
+  .get(getSingleCourse)
+  .put(updateSingleCourse)
+  .delete(deleteSingleCourse);
 
 module.exports = router;
