de7fe07567d78145e52b84d1bb7e9656ce1e679c
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2022-01-21 01:52:34 -0600

    Made middleware for sharing logic between controller methods

--- routes/courses.js
+++ routes/courses.js
@@ -7,9 +7,21 @@ const {
   deleteSingleCourse,
 } = require('../controllers/courses');
 
+const Course = require('../models/Course');
+const advanceResults = require('../middleware/advanceResults');
+
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses).post(addSingleCourse);
+router
+  .route('/')
+  .get(
+    advanceResults(Course, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getCourses
+  )
+  .post(addSingleCourse);
 router
   .route('/:id')
   .get(getSingleCourse)
