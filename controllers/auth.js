18b7899a5f47bac57b2ae40ccabd417768ed66b8
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2022-01-22 01:08:11 -0600

    Adding cookie parser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 // const crypto = require('crypto');
-const ErrorClass = require('../utils/errorClass');
+// const ErrorClass = require('../utils/errorClass');
 const asyncHandler = require('../middleware/async');
 // const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
@@ -17,13 +17,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({
-    success: true,
-    token,
-  });
+  sendTokeyRes(user, 200, res);
 });
 
 // @desc      Login user
@@ -51,11 +45,27 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
+  sendTokeyRes(user, 200, res);
+});
+
+// Get token from model also creates cookie and sends response
+const sendTokeyRes = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({
-    success: true,
-    token,
-  });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({ success: true, token });
+};
